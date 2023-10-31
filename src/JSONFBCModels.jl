88d4f8547d3362c2fda27e9c51e3f4a7f52f0e4c
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2023-10-31 11:33:00 +0100

    clean up imports&exports

--- src/JSONFBCModels.jl
+++ src/JSONFBCModels.jl
@@ -1,14 +1,15 @@
 module JSONFBCModels
 
 using DocStringExtensions
+
 import AbstractFBCModels as A
-using JSON, SparseArrays
+
+using JSON
+using SparseArrays
 
 include("constants.jl")
 include("utils.jl")
 include("interface.jl")
 include("io.jl")
-
-export JSONFBCModel
 
 end
